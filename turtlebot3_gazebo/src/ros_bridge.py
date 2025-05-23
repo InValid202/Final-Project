af09b87607b1730ec8a801bfe24fbb79236b2278
InValid202 <rae6452500368@gmail.com>
2025-05-23 17:35:25 +0700

    update

--- turtlebot3_gazebo/src/ros_bridge.py
+++ turtlebot3_gazebo/src/ros_bridge.py
@@ -5,6 +5,18 @@ import rospy
 import threading
 from std_msgs.msg import String
 
+stored_orders = []
+
+table_locations = {
+    "original": {'x_position': 0, 'y_position': 0, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 0.0, 'w_orientation': 1.0},
+    "Table 1": {'x_position': -1, 'y_position': -10.35, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 0.0, 'w_orientation': 1.0},
+    "Table 2": {'x_position': -1, 'y_position': -12.8, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 0.0, 'w_orientation': 1.0},
+    "Table 3": {'x_position': -1, 'y_position': -15.3, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 0.0, 'w_orientation': 1.0},
+    "Table 4": {'x_position': -31, 'y_position': -10.35, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 1.0, 'w_orientation': 0.0},
+    "Table 5": {'x_position': -31, 'y_position': -12.8, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 1.0, 'w_orientation': 0.0},
+    "Table 6": {'x_position': -31, 'y_position': -15.3, 'z_position': 0.188798, 'x_orientation': 0.0, 'y_orientation': 0.0, 'z_orientation': 1.0, 'w_orientation': 0.0},
+}
+
 app = Flask(__name__)
 CORS(app)
 
@@ -13,7 +25,7 @@ pub = rospy.Publisher('/order_stage_topic', String, queue_size=10)
 
 @app.route("/")
 def home():
-    return "Hello, This is Local Host for ROS to receive Message From ROS."
+    return "Hello, This is Local Host for ROS to receive Message From Odoo."
 
 @app.route('/ros/order_stage', methods=['POST'])
 def handle_order_stage():
@@ -35,5 +47,5 @@ def handle_order_stage():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
-    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
+    app.run(host="0.0.0.0", port=5000, ssl_context=("/home/beer/tumpoom_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/cert.pem", "/home/beer/tumpoom_ws/src/turtlebot3_simulations/turtlebot3_gazebo/src/key.pem"))
 
